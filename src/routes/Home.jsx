4d1ae6510dd02c4b12126a6552c2cd43f82f1307
rosemutai <chepngetichrose2030@gmail.com>
2023-05-03 18:17:11 +0300

    Add homepage route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,10 @@
+import Books from "../components/Books";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home-page w-10/12 mx-auto bg-white h-full">
+      <Books />
+    </div>
   )
 }
 
