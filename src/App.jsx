51b734b49bcd2718dc8eca20ba6667f3cfb7dd6c
rosemutai <chepngetichrose2030@gmail.com>
2023-05-03 18:16:09 +0300

    Add project routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Categories from './routes/Categories';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="h">
+    <div className="w-full bg-slate-100 h-screen py-16">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="categories" element={<Categories />}></Route>
