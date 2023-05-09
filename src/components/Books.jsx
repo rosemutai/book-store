6db69bfc0962e87212dbe216b53dba7ff6703445
rosemutai <chepngetichrose2030@gmail.com>
2023-05-09 10:58:07 +0300

    Get initial book state from books slice

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,41 +1,20 @@
+import { useSelector } from "react-redux";
 import AddBook from "./AddBook";
 import Book from "./Book"
 
-const arrBooks = [
-  {
-    category: "Fiction",
-    title: "The Red Lights",
-    author: "Rose Roberts",
-    comments: ["Great Book", "Love it!"],
-  },
-
-  {
-    category: "Science Fiction",
-    title: "Dune",
-    author: "Frank Herbert",
-    comments: ["Great Book", "Love it!"],
-  },
-
-  {
-    category: "Econmy",
-    title: "Capital in the Twenty-First Century",
-    author: "Suzanne Collins",
-    comments: ["Great Book", "Love it!"],
-  },
-];
-
 const Books = () => {
+
+  const { books } = useSelector((state) => state.books)
   return (
     <div className="">
       <div className="books-list">
-        {arrBooks.map((book) => {
+        {books.map((book) => {
           return (
             <div key={book.title}>
               <Book
                 category={book.category}
                 title={book.title}
                 author={book.author}
-                comments={book.comments}
               />
             </div>
           );
