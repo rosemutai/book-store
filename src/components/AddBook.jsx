91d84026810bd9668fcb0712e33a6d9f68228ed4
rosemutai <chepngetichrose2030@gmail.com>
2023-05-09 15:22:59 +0300

    Add book using form

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,29 +1,38 @@
+import { useState } from 'react';
 import { useDispatch } from "react-redux";
 import { addBook } from "../redux/books/booksSlice";
+import Button from './Button';
 
 const AddBook = () => {
 
   const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  // const books = useSelector((state) => state.books.books)
+
+  const resetForm = () => {
+    setTitle("");
+    setAuthor("");
+  }
 
   const submitForm = (e) => {
     e.preventDefault();
-
     const item_id = Math.floor(Math.random(0) * 1000)
-    const title = e.target.elements.title.value
-    const author = e.target.elements.author.value;
 
     dispatch(addBook({ item_id, title, author }));
+    resetForm();
   }
 
   return (
     <div className="add-book-form">
       <h3 className="form-title">AddBook</h3>
-      <form className="form" onSubmit={submitForm}>
+      <form className="form">
         <input
           type="text"
           id="title-input"
           placeholder="Book title"
           name="title"
+          onChange={(e) => setTitle(e.target.value)}
           className="border border-slate-200"
         />
 
@@ -32,12 +41,11 @@ const AddBook = () => {
           id="author-input"
           placeholder="Author"
           name="author"
+          onChange={(e) => setAuthor(e.target.value)}
           className="border border-slate-200"
         />
 
-        <button type="submit" className="bg-turquoise text-white">
-          ADD BOOK
-        </button>
+        <Button text="ADD BOOK" onClick={submitForm}  />
       </form>
     </div>
   );
