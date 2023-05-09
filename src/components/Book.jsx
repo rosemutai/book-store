a22c9f98a804fb08aab54f8a687a0b55ca113788
rosemutai <chepngetichrose2030@gmail.com>
2023-05-09 10:56:37 +0300

    Remove undefined props

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,16 +1,11 @@
 import { PropTypes } from "prop-types";
 
-const Book = ({ category, title, author, comments }) => {
+const Book = ({ category, title, author }) => {
   return (
     <div className="book">
       <h4>{category}</h4>
       <h2>{title}</h2>
       <p>{author}</p>
-      <ul>
-        {comments.map((comment) => {
-          <li key={comment}>{comment}</li>
-        })}
-      </ul>
       <button type="button" className="bg-black text-white">Delete</button>
     </div>
   );
