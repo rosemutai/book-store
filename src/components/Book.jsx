46d1b7d74cac9138822d96f20644889caed26042
rosemutai <chepngetichrose2030@gmail.com>
2023-05-09 12:03:31 +0300

    Remove a single book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,30 @@
 import { PropTypes } from "prop-types";
+import { useDispatch } from "react-redux";
+import { removeBook } from "../redux/books/booksSlice";
 
-const Book = ({ category, title, author }) => {
+const Book = ({ item_id, category, title, author }) => {
+  
+  let dispatch = useDispatch();
+
   return (
     <div className="book">
       <h4>{category}</h4>
       <h2>{title}</h2>
       <p>{author}</p>
-      <button type="button" className="bg-black text-white">Delete</button>
+      <button
+        className="bg-black text-white"
+        onClick={() => dispatch(removeBook(item_id))}
+      >Remove</button>
+
     </div>
   );
 };
 
 Book.propTypes = {
+  item_id: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   comments: PropTypes.array,
-
 };
 export default Book
