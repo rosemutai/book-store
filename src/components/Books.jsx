64d0c74e719ffb070231172b58c41c4f793d1d4d
rosemutai <chepngetichrose2030@gmail.com>
2023-05-09 15:25:20 +0300

    Delete book

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,28 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AddBook from "./AddBook";
 import Book from "./Book"
+import { removeBook } from "../redux/books/booksSlice";
 
 const Books = () => {
 
   const { books } = useSelector((state) => state.books)
+  let dispatch = useDispatch();
+
+    const deleteBook = (e) => {
+      dispatch(removeBook(e.target.id));
+    };
+
   return (
     <div className="">
       <div className="books-list">
-        {books.map((book) => {
-          return <Book key={book.item_id} {...book} />
+        {Array.from(books).map((book) => {
+          return (
+            <Book
+              key={book.item_id}
+              {...book}
+              onClick={deleteBook}
+            />
+          );
         })}
       </div>
 
