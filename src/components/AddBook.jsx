79a7541f9bd5a7a17ff6eda82d8286cc5f707c69
rosemutai <chepngetichrose2030@gmail.com>
2023-05-09 13:32:52 +0300

    Use addBook reducer to add a book

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,49 +1,39 @@
-import { useState } from "react"
+import { useDispatch } from "react-redux";
+import { addBook } from "../redux/books/booksSlice";
 
 const AddBook = () => {
 
-  const [state, setState] = useState({
-    title: "",
-    bookCategory: "",
+  const dispatch = useDispatch();
 
-  });
+  const submitForm = (e) => {
+    e.preventDefault();
 
-  const handleChange = (e) => {
-    setState((state) => ({
-      ...state,
-      [e.target.name]: e.target.value,
-    }));
+    const item_id = Math.floor(Math.random(0) * 1000)
+    const title = e.target.elements.title.value
+    const author = e.target.elements.author.value;
+
+    dispatch(addBook({ item_id, title, author }));
   }
 
-  const categories = ["Science Fiction", "Economy"]
-  const categoryOptions = categories.map((category) => (
-    <option value={category} key={category}>
-      {category}
-    </option>
-  ))
   return (
     <div className="add-book-form">
       <h3 className="form-title">AddBook</h3>
-      <form className="form">
+      <form className="form" onSubmit={submitForm}>
         <input
           type="text"
           id="title-input"
           placeholder="Book title"
-          value={state.title}
-          onChange={handleChange}
+          name="title"
           className="border border-slate-200"
         />
-        <select
-          name="book-categories"
-          value={state.bookCategory}
-          onChange={handleChange}
-          className="border border-slate-200 ml-5"
-        >
-          <option value={""} disabled>
-            Category
-          </option>
-          {categoryOptions}
-        </select>
+
+        <input
+          type="text"
+          id="author-input"
+          placeholder="Author"
+          name="author"
+          className="border border-slate-200"
+        />
 
         <button type="submit" className="bg-turquoise text-white">
           ADD BOOK
