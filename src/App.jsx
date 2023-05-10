dd969c9c70cdd2050652a0db0c0cfa287112c2f6
rosemutai <chepngetichrose2030@gmail.com>
2023-05-10 15:39:48 +0300

    Remove fetch Function from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import Home from "./routes/Home";
 import Categories from "./routes/Categories";
 import Navbar from "./components/Navbar";
-import { fetchBooks } from "./redux/books/booksSlice";
 
 function App() {
 
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchBooks())
-  }, [dispatch]);
   return (
     <div className="w-full bg-slate-100 h-screen py-16">
       <Navbar />
