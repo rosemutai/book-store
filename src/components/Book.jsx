fddc5bf4ceeec4362060379518e156da5a1ea118
rosemutai <chepngetichrose2030@gmail.com>
2023-05-10 15:41:49 +0300

    Remove unused variables

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,12 +1,10 @@
 import { PropTypes } from "prop-types";
 import Button from "./Button";
 
-
 const Book = ({ item_id, title, author, onClick }) => {
 
   return (
     <div className="book">
-      {/* <h4>{category}</h4> */}
       <h2>{title}</h2>
       <p>{author}</p>
 
@@ -22,7 +20,6 @@ const Book = ({ item_id, title, author, onClick }) => {
 
 Book.propTypes = {
   item_id: PropTypes.string,
-  // category: PropTypes.string,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   comments: PropTypes.array,
