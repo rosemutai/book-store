52fa7cfd55e3520c7538413bd94aa92d09116d81
rosemutai <chepngetichrose2030@gmail.com>
2023-05-11 07:37:00 +0300

    Add padding

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ import Books from "../components/Books";
 
 const Home = () => {
   return (
-    <div className="home-page w-10/12 mx-auto bg-white h-full">
+    <div className="home-page w-10/12 mx-auto bg-white h-full mt-5 px-20">
       <Books />
     </div>
   )
