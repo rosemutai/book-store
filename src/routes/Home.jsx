69f4718e7a9e6deee06d2df6bb4b0e93ca09634a
rosemutai <chepngetichrose2030@gmail.com>
2023-05-11 09:42:03 +0300

    Change background color

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ import Books from "../components/Books";
 
 const Home = () => {
   return (
-    <div className="home-page w-10/12 mx-auto bg-white h-full mt-5 px-20">
+    <div className="home-page w-10/12 mx-auto bg-slate-50 h-full py-8 px-20">
       <Books />
     </div>
   )
