28b6e53ac77bb811ecec900c29a9aa2c432aaba2
rosemutai <chepngetichrose2030@gmail.com>
2023-05-20 15:22:58 +0300

    Make page responsive

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,10 @@ import Button from "./Button";
 const Book = ({ item_id, title, author, onClick }) => {
 
   return (
-    <div className="book bg-white my-3 p-4 border border-slate-200 rounded-sm flex justify-between">
+    <div
+      className="book bg-white my-3 p-4 border border-slate-200 
+      rounded-sm flex md:flex-row flex-col justify-between w-full"
+    >
       <div className="left-section">
         <h5 className="text-gray-400/80 font-bold">Action</h5>
         <h2 className=" text-black text-lg font-bold">{title}</h2>
@@ -34,9 +37,9 @@ const Book = ({ item_id, title, author, onClick }) => {
         </div>
       </div>
 
-      <div className="right-section flex">
-        <div className="progress flex border-y-0 border-l-0 border-r-2 border-r-slate-300/60 pr-8">
-          <div className="progress-bar">
+      <div className="right-section flex md:flex-row flex-col">
+        <div className="progress flex border-y-0 border-l-0 md:border-r-2 border-r-slate-300/60 pr-0 md:pr-8 my-3 md:my-0">
+          <div className="progress-bar ">
             <progress value="75" min="0" max="100" className="progress-circle">
               64%
             </progress>
@@ -46,11 +49,13 @@ const Book = ({ item_id, title, author, onClick }) => {
             <h4 className="percentage text-2xl font-normal tracking-wide text-black/80">
               64%
             </h4>
-            <p className="completed-status text-gray-400/70 font-normal">Completed</p>
+            <p className="completed-status text-gray-400/70 font-normal">
+              Completed
+            </p>
           </div>
         </div>
 
-        <div className="chapter px-8">
+        <div className="chapter mt-2 md:mt-0 md:px-8">
           <h5 className="current-chapter-title text-xs text-gray-400/70">
             CURRENT CHAPTER
           </h5>
