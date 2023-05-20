f46eb809a1e45ff71708ec70a8c95908721832e2
rosemutai <chepngetichrose2030@gmail.com>
2023-05-20 14:51:43 +0300

    Make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,86 @@
+import { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Transition } from "@headlessui/react";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar w-10/12 mx-auto shadow-md px-16 py-5 bg-white flex justify-between rounded-sm">
-      <div className="menu-links flex gap-12 items-center">
-        <Link to="/" className="logo text-turquoise font-bold text-3xl">
-          Bookstore CMS
-        </Link>
-        <NavLink to="/" className="uppercase tracking-wider text-slate-400">
-          BOOKS
-        </NavLink>
-        <NavLink
-          to="categories"
-          className="uppercase tracking-wider text-slate-400"
-        >
-          CATEGORIES
-        </NavLink>
-      </div>
+  const [isShowing, setIsShowing] = useState(false);
 
-      <div className="user">
-        <FontAwesomeIcon
-          icon={faUser}
-          className="text-turquoise border border-slate-200 p-4 rounded-full"
-        />
+  return (
+    <nav className="navbar w-11/12 md:w-10/12 mx-auto shadow-md px-4 md:px-16 py-5 bg-white flex justify-between rounded-sm">
+      <div className="menu-links flex gap-12 items-center">
+        <div className="logo flex justify-between items-center">
+          <Link
+            to="/"
+            className="logo text-turquoise font-bold text-sm md:text-3xl"
+          >
+            Bookstore CMS
+          </Link>
+          <button
+            type="button"
+            onClick={() => setIsShowing((isShowing) => !isShowing)}
+            className="cursor-pointer ml-40"
+          >
+            {isShowing ? (
+              <FontAwesomeIcon
+                icon={faTimes}
+                className="text-turquoise md:hidden block"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                className="text-turquoise md:hidden block"
+              />
+            )}
+          </button>
+        </div>
+        <div className="other-navlinks hidden md:flex gap-6 items-center">
+          <NavLink to="/" className="uppercase tracking-wider text-slate-400">
+            BOOKS
+          </NavLink>
+          <NavLink
+            to="categories"
+            className="uppercase tracking-wider text-slate-400"
+          >
+            CATEGORIES
+          </NavLink>
+
+          <FontAwesomeIcon
+            icon={faUser}
+            className="text-turquoise border border-slate-200 p-4 rounded-full ml-96"
+          />
+        </div>
+
+        <Transition
+          show={isShowing}
+          enter="transition-opacity duration-75"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            className="other-navlinks md:hidden flex flex-col gap-12 absolute z-30
+            top-36 left-4 bg-turquoise w-11/12 mx-auto h-screen pl-3"
+          >
+            <NavLink to="/" className="uppercase tracking-wider text-white pt-8">
+              BOOKS
+            </NavLink>
+            <NavLink
+              to="categories"
+              className="uppercase tracking-wider text-white"
+            >
+              CATEGORIES
+            </NavLink>
+
+            <FontAwesomeIcon
+              icon={faUser}
+              className="text-slate-200 border border-slate-200 p-4 rounded-full h-5 w-5"
+            />
+          </div>
+        </Transition>
       </div>
     </nav>
   );
