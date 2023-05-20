8f054817a8054135e30bebbb46057f74f609fc12
rosemutai <chepngetichrose2030@gmail.com>
2023-05-20 15:38:21 +0300

    Make page responsive

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,7 @@ import AddBook from "../components/AddBook";
 
 const Home = () => {
   return (
-    <div className="home-page w-10/12 mx-auto bg-slate-50 py-8 px-20 shadow-xl">
+    <div className="home-page w-11/12 md:w-10/12 mx-auto bg-slate-50 py-8 px-4 md:px-20 shadow-xl">
       <Books />
 
       <div className="form-section">
